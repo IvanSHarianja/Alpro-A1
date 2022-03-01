2e3507222f749a569b09e27ab3e072496a805870
IvanSHarianja <100180168+IvanSHarianja@users.noreply.github.com>
2022-03-01 11:56:50 +0700

    Add files via upload

---
+++ Praktikum2/luasbjsk.c
@@ -0,0 +1,20 @@
+/*Nama File	: luasbjsk.c*/
+/*Deskripsi	: menghitung dan menampilkan luas bujur sangkar*/
+/*Pembuat	: Ivan S Harianja - 24060121120007*/
+/*Tgl pembuatan	: Selasa, 01 Maret 2022*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*Kamus*/
+    int S;
+    int Luas;
+
+/*Algoritma*/
+   scanf("%d",&S);
+   Luas = (S*S);
+
+   printf ("Luas Bujursangkar = %d", Luas);
+
+}
